ab2180e4a6e8f6ee1052b2eb51fde58a49c9c931
Kyle Laplante <kyle.laplante@gmail.com>
2012-11-20 19:51:10 -0800

    remove useless feature * wipe battery stats is a myth * detailed post: https://plus.google.com/u/0/105051985738280261832/posts/FV3LVtdVxPT

--- extendedcommands.c
+++ extendedcommands.c
@@ -1190,14 +1190,6 @@ void show_nandroid_menu()
     }
 }
 
-void wipe_battery_stats()
-{
-    ensure_path_mounted("/data");
-    remove("/data/system/batterystats.bin");
-    ensure_path_unmounted("/data");
-    ui_print("Battery Stats wiped.\n");
-}
-
 static void partition_sdcard(const char* volume) {
     if (!can_partition(volume)) {
         ui_print("Can't partition device: %s\n", volume);
@@ -1277,7 +1269,6 @@ void show_advanced_menu()
     static char* list[] = { "reboot recovery",
 			    "reboot download",
                             "wipe dalvik cache",
-                            "wipe battery stats",
                             "report error",
                             "key test",
                             "show log",
@@ -1313,13 +1304,9 @@ void show_advanced_menu()
                 ensure_path_unmounted("/data");
                 break;
             case 3:
-                if (confirm_selection( "Confirm wipe?", "Yes - Wipe Battery Stats"))
-                    wipe_battery_stats();
-                break;
-            case 4:
                 handle_failure(1);
                 break;
-            case 5:
+            case 4:
             {
                 ui_print("Outputting key codes.\n");
                 ui_print("Go back to end debugging.\n");
@@ -1334,10 +1321,10 @@ void show_advanced_menu()
                 while (action != GO_BACK);
                 break;
             }
-            case 6:
+            case 5:
                 ui_printlogtail(12);
                 break;
-            case 7:
+            case 6:
                 ensure_path_mounted("/system");
                 ensure_path_mounted("/data");
                 ensure_path_mounted("/emmc");
@@ -1345,7 +1332,7 @@ void show_advanced_menu()
                 __system("fix_permissions");
                 ui_print("Done!\n");
                 break;
-	    case 8:
+	    case 7:
                	ensure_path_mounted("/system");
                 ensure_path_mounted("/data");
                 ui_print("Fixing permissions & removing stale directories (logging disabled)...\n");
